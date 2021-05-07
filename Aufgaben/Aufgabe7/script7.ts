119be2de8d77ef2aac9513d9a2ba54acac3b8713
Hannah Sättele <hannah.saettele@hs-furtwangen.de>
2021-05-07 15:30:41 +0200

    A7 bearbeiten

--- Aufgaben/Aufgabe7/script7.ts
+++ Aufgaben/Aufgabe7/script7.ts
@@ -1,9 +1,26 @@
-var sound:HTMLAudioElement = new Audio('L07_A.mp3'); sound.play();
-var sound:HTMLAudioElement = new Audio('L07_C.mp3'); sound.play();
-var sound:HTMLAudioElement = new Audio('L07_F.mp3'); sound.play();
-var sound:HTMLAudioElement = new Audio('L07_G.mp3'); sound.play();
-var sound:HTMLAudioElement = new Audio('L07_hithat.mp3'); sound.play();
-var sound:HTMLAudioElement = new Audio('L07_kick.mp3'); sound.play();
-var sound:HTMLAudioElement = new Audio('L07_laugh-1.mp3'); sound.play();
-var sound:HTMLAudioElement = new Audio('L07_laugh-2.mp3'); sound.play();
-var sound:HTMLAudioElement = new Audio('L07_snare.mp3'); sound.play();
+window.addEventListener("load", function (){
+
+function playSample(samples: string): void {
+    var sound:HTMLAudioElement = new Audio('L07_A.mp3'); sound.play();
+};
+
+
+document.querySelector("#kick").addEventListener("mousedown", function(){playSample("L07_kick.mp3");});
+document.querySelector("#snare").addEventListener("mousedown", function(){playSample("L07_snare.mp3");});
+document.querySelector("#hithat").addEventListener("mousedown", function(){playSample("L07_hihat.mp3");});
+document.querySelector("#noteA").addEventListener("mousedown", function(){playSample("L07_A.mp3");});
+document.querySelector("#noteC").addEventListener("mousedown", function(){playSample("L07_C.mp3");});
+document.querySelector("#noteF").addEventListener("mousedown", function(){playSample("L07_F.mp3");});
+document.querySelector("#noteG").addEventListener("mousedown", function(){playSample("L07_G.mp3");});
+document.querySelector("#laugh1").addEventListener("mousedown", function(){playSample("L07_laugh-1.mp3");});
+document.querySelector("#laugh2").addEventListener("mousedown", function(){playSample("L07_laugh-2.mp3");});
+
+document.querySelector("#play").addEventListener("click", play)});
+
+function playSample {
+
+}
+
+function play {
+    
+}
