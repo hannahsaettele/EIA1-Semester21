5a58e4545d730be78bb1c89d93f2d0a3f729ac6d
Hannah Sättele <hannah.saettele@hs-furtwangen.de>
2021-05-07 16:55:58 +0200

    A7 bearbeiten

--- Aufgaben/Aufgabe5/script.ts
+++ Aufgaben/Aufgabe5/script.ts
@@ -1,38 +1,37 @@
+var europa18: number = 4209.3;
+var europa08: number = 4965.7;
+var afrika18: number = 1235.5;
+var afrika08: number = 1028;
+var südamerika18: number = 1261.5;
+var südamerika08: number = 1132.6;
+var nordamerika18: number = 6035.6;
+var nordamerika08: number = 6600.4;
+var asien18: number = 16274.1;
+var asien08: number = 12954.7;
+var australien18: number = 1993;
+var australien08: number = 2100.5;
 
-let europa18: number = 4209.3;
-let europa08: number = 4965.7;
-let afrika18: number = 1235.5;
-const afrika08: number = 1028;
-let südamerika18: number = 1261.5;
-let südamerika08: number = 1132.6;
-let nordamerika18: number = 6035.6;
-let nordamerika08: number = 6600.4;
-let asien18: number = 16274.1;
-let asien08: number = 12954.7;
-let australien18: number = 1993;
-let australien08: number = 2100.5;
+var world = europa18 + afrika18 + südamerika18 + nordamerika18 + asien18 + australien18;
+var relativeuropa = Math.round(europa18 / world * 100 * 100) / 100;
+var relativafrika =  Math.round(afrika18 / world * 100 * 100) / 100;
+var relativsüdamerika = Math.round(südamerika18 / world * 100 * 100) / 100;
+var relativnordamerika = Math.round(nordamerika18 / world * 100 * 100) / 100;
+var relativasien = Math.round(asien18 / world * 100 * 100) / 100;
+var relativaustralien = Math.round(australien18 / world * 100 * 100) / 100;
 
-let world = europa18 + afrika18 + südamerika18 + nordamerika18 + asien18 + australien18;
-let relativeuropa = Math.round(europa18 / world * 100 * 100) / 100;
-let relativafrika =  Math.round(afrika18 / world * 100 * 100) / 100;
-let relativsüdamerika = Math.round(südamerika18 / world * 100 * 100) / 100;
-let relativnordamerika = Math.round(nordamerika18 / world * 100 * 100) / 100;
-let relativasien = Math.round(asien18 / world * 100 * 100) / 100;
-let relativaustralien = Math.round(australien18 / world * 100 * 100) / 100;
+var differenzeuropa = Math.round((europa18 - europa08) / europa08 * 100 * 100) / 100;
+var differenzafrika = Math.round((afrika18 - afrika08) / afrika08 * 100 * 100) / 100;
+var differenzsüdamerika = Math.round((südamerika18 - südamerika08) / südamerika08 * 100 * 100) / 100;
+var differenznordamerika = Math.round((nordamerika18 - nordamerika08) / nordamerika08 * 100 * 100) / 100;
+var differenzasien = Math.round((asien18 - asien08) / asien08 * 100 * 100) / 100;
+var differenzaustralien = Math.round((australien18 - australien08) / australien08 * 100 * 100) / 100;
 
-let differenzeuropa = Math.round((europa18 - europa08) / europa08 * 100 * 100) / 100;
-let differenzafrika = Math.round((afrika18 - afrika08) / afrika08 * 100 * 100) / 100;
-let differenzsüdamerika = Math.round((südamerika18 - südamerika08) / südamerika08 * 100 * 100) / 100;
-let differenznordamerika = Math.round((nordamerika18 - nordamerika08) / nordamerika08 * 100 * 100) / 100;
-let differenzasien = Math.round((asien18 - asien08) / asien08 * 100 * 100) / 100;
-let differenzaustralien = Math.round((australien18 - australien08) / australien08 * 100 * 100) / 100;
-
-let emissioneuropa = Math.round(europa18 - europa08 * 100) / 100;
-let emissionafrika = Math.round(afrika18 - afrika08 * 100) / 100;
-let emissionsüdamerika = Math.round(südamerika18 - südamerika08 * 100) / 100;
-let emissionnordamerika = Math.round(nordamerika18 - nordamerika08 * 100) / 100; 
-let emissionasien = Math.round(asien18 - asien08 * 100) / 100;
-let emissionaustralien = Math.round(australien18 - australien08 * 100) / 100;
+var emissioneuropa = Math.round(europa18 - europa08 * 100) / 100;
+var emissionafrika = Math.round(afrika18 - afrika08 * 100) / 100;
+var emissionsüdamerika = Math.round(südamerika18 - südamerika08 * 100) / 100;
+var emissionnordamerika = Math.round(nordamerika18 - nordamerika08 * 100) / 100; 
+var emissionasien = Math.round(asien18 - asien08 * 100) / 100;
+var emissionaustralien = Math.round(australien18 - australien08 * 100) / 100;
 
 
 
--- Aufgaben/Aufgabe7/script7.js
+++ Aufgaben/Aufgabe7/script7.js
@@ -1,22 +1,18 @@
-window.addEventListener("load", function () {
-    function playSample(samples) {
-        var sound = new Audio('sample');
-        sound.play();
-    }
-    ;
-    document.querySelector("#kick").addEventListener("mousedown", function () { playSample("L07_kick.mp3"); });
-    document.querySelector("#snare").addEventListener("mousedown", function () { playSample("L07_snare.mp3"); });
-    document.querySelector("#hithat").addEventListener("mousedown", function () { playSample("L07_hihat.mp3"); });
-    document.querySelector("#noteA").addEventListener("mousedown", function () { playSample("L07_A.mp3"); });
-    document.querySelector("#noteC").addEventListener("mousedown", function () { playSample("L07_C.mp3"); });
-    document.querySelector("#noteF").addEventListener("mousedown", function () { playSample("L07_F.mp3"); });
-    document.querySelector("#noteG").addEventListener("mousedown", function () { playSample("L07_G.mp3"); });
-    document.querySelector("#laugh1").addEventListener("mousedown", function () { playSample("L07_laugh-1.mp3"); });
-    document.querySelector("#laugh2").addEventListener("mousedown", function () { playSample("L07_laugh-2.mp3"); });
-    document.querySelector("#play").addEventListener("click", play);
-});
-function playSample() {
+function playSample(samples) {
+    var sound = new Audio('sample');
+    sound.play();
 }
+;
+document.querySelector("#kick").addEventListener("mousedown", function () { playSample("L07_kick.mp3"); });
+document.querySelector("#snare").addEventListener("mousedown", function () { playSample("L07_snare.mp3"); });
+document.querySelector("#hithat").addEventListener("mousedown", function () { playSample("L07_hihat.mp3"); });
+document.querySelector("#noteA").addEventListener("mousedown", function () { playSample("L07_A.mp3"); });
+document.querySelector("#noteC").addEventListener("mousedown", function () { playSample("L07_C.mp3"); });
+document.querySelector("#noteF").addEventListener("mousedown", function () { playSample("L07_F.mp3"); });
+document.querySelector("#noteG").addEventListener("mousedown", function () { playSample("L07_G.mp3"); });
+document.querySelector("#laugh1").addEventListener("mousedown", function () { playSample("L07_laugh-1.mp3"); });
+document.querySelector("#laugh2").addEventListener("mousedown", function () { playSample("L07_laugh-2.mp3"); });
+document.querySelector("#play").addEventListener("click", function () { play(); });
 function play() {
 }
 //# sourceMappingURL=script7.js.map
