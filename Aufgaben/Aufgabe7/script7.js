58293c0dba5b6ae4ec203043696a9a36d618e78f
Hannah Sättele <hannah.saettele@hs-furtwangen.de>
2021-05-10 00:08:51 +0200

    a7

--- Aufgaben/Aufgabe7/script7.js
+++ Aufgaben/Aufgabe7/script7.js
@@ -1,18 +1,56 @@
-function playSample(samples) {
-    var sound = new Audio('sample');
-    sound.play();
-}
-;
-document.querySelector("#kick").addEventListener("mousedown", function () { playSample("L07_kick.mp3"); });
-document.querySelector("#snare").addEventListener("mousedown", function () { playSample("L07_snare.mp3"); });
-document.querySelector("#hithat").addEventListener("mousedown", function () { playSample("L07_hihat.mp3"); });
-document.querySelector("#noteA").addEventListener("mousedown", function () { playSample("L07_A.mp3"); });
-document.querySelector("#noteC").addEventListener("mousedown", function () { playSample("L07_C.mp3"); });
-document.querySelector("#noteF").addEventListener("mousedown", function () { playSample("L07_F.mp3"); });
-document.querySelector("#noteG").addEventListener("mousedown", function () { playSample("L07_G.mp3"); });
-document.querySelector("#laugh1").addEventListener("mousedown", function () { playSample("L07_laugh-1.mp3"); });
-document.querySelector("#laugh2").addEventListener("mousedown", function () { playSample("L07_laugh-2.mp3"); });
-document.querySelector("#play").addEventListener("click", function () { play(); });
-function play() {
-}
+window.addEventListener("load", function () {
+    document.querySelector(".kick").addEventListener("click", function () { beats("L07_kick.mp3"); });
+    document.querySelector(".hithat").addEventListener("click", function () { beats("L07_hihat.mp3"); });
+    document.querySelector(".snare").addEventListener("click", function () { beats("L07_snare.mp3"); });
+    document.querySelector(".noteA").addEventListener("click", function () { beats("L07_A.mp3"); });
+    document.querySelector(".noteC").addEventListener("click", function () { beats("L07_C.mp3"); });
+    document.querySelector(".noteF").addEventListener("click", function () { beats("L07_F.mp3"); });
+    document.querySelector(".noteG").addEventListener("click", function () { beats("L07_G.mp3"); });
+    document.querySelector(".laugh1").addEventListener("click", function () { beats("L07_laugh-1.mp3"); });
+    document.querySelector(".laugh2").addEventListener("click", function () { beats("L07_laugh-2.mp3"); });
+    function beats(music) {
+        var sound = new Audio(music);
+        sound.play();
+    }
+    ;
+    var sounds = [new Audio("L07_kick.mp3"), new Audio("L07_hihat.mp3"), new Audio("L07_snare.mp3"), new Audio("L07_A.mp3"), new Audio("L07_C.mp3"), new Audio("L07_F.mp3"), new Audio("L07_G.mp3"), new Audio("L07_laugh-1.mp3"), new Audio("L07_laugh-2.mp3")];
+    function intervall() {
+        setTimeout(function () {
+            sounds[0].play();
+        }, 500);
+        setTimeout(function () {
+            sounds[1].play();
+        }, 1000);
+        setTimeout(function () {
+            sounds[2].play();
+        }, 1500);
+        setTimeout(function () {
+            sounds[1].play();
+        }, 2000);
+        setTimeout(function () {
+            sounds[0].play();
+        }, 2500);
+        setTimeout(function () {
+            sounds[1].play();
+        }, 3000);
+        setTimeout(function () {
+            sounds[2].play();
+        }, 3500);
+        setTimeout(function () {
+            sounds[1].play();
+        }, 4000);
+    }
+    ;
+    document.querySelector(".play").addEventListener("click", function () {
+        intervall();
+    });
+    function keyBoard(event) {
+        var keys = event.keyBoard;
+        if (keys == 49) {
+            beats("L07_kick.mp3");
+        }
+        ;
+    }
+    ;
+});
 //# sourceMappingURL=script7.js.map
