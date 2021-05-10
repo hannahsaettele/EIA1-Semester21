b8e6ecda05b5604f46bf53b2ff32db03127b8a7b
Hannah Sättele <hannah.saettele@hs-furtwangen.de>
2021-05-10 12:07:18 +0200

    a7

--- Aufgaben/Aufgabe7/script7.ts
+++ Aufgaben/Aufgabe7/script7.ts
@@ -1,15 +1,15 @@
 window.addEventListener("load", function (){
 
-    document.querySelector("#Sound1").addEventListener("mousedown", function(){playSample("L07_kick.mp3");});
-    document.querySelector("#Sound2").addEventListener("mousedown", function(){playSample("L07_hihat.mp3");});
-    document.querySelector("#Sound3").addEventListener("mousedown", function(){playSample("L07_snare.mp3");});
-    document.querySelector("#Sound4").addEventListener("mousedown", function(){playSample("L07_A.mp3");});
-    document.querySelector("#Sound5").addEventListener("mousedown", function(){playSample("L07_C.mp3");});
-    document.querySelector("#Sound6").addEventListener("mousedown", function(){playSample("L07_F.mp3");});
-    document.querySelector("#Sound7").addEventListener("mousedown", function(){playSample("L07_G.mp3");});
-    document.querySelector("#Sound8").addEventListener("mousedown", function(){playSample("L07_laugh-1.mp3");});
-    document.querySelector("#Sound9").addEventListener("mousedown", function(){playSample("L07_laugh-2.mp3");});
-    document.querySelector("#PlayButton").addEventListener("click", Play)});
+    document.querySelector("#kick").addEventListener("mousedown", function(){playSample("L07_kick.mp3");});
+    document.querySelector("#hithat").addEventListener("mousedown", function(){playSample("L07_hihat.mp3");});
+    document.querySelector("#snare").addEventListener("mousedown", function(){playSample("L07_snare.mp3");});
+    document.querySelector("#noteA").addEventListener("mousedown", function(){playSample("L07_A.mp3");});
+    document.querySelector("#noteC").addEventListener("mousedown", function(){playSample("L07_C.mp3");});
+    document.querySelector("#noteF").addEventListener("mousedown", function(){playSample("L07_F.mp3");});
+    document.querySelector("#noteG").addEventListener("mousedown", function(){playSample("L07_G.mp3");});
+    document.querySelector("#laugh1").addEventListener("mousedown", function(){playSample("L07_laugh-1.mp3");});
+    document.querySelector("#laugh2").addEventListener("mousedown", function(){playSample("L07_laugh-2.mp3");});
+    document.querySelector("#play").addEventListener("click", Play)});
 
 function playSample(samples: string): void{
 var sound:HTMLAudioElement = new Audio(samples);
