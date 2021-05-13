2e71235ecd451ab93a31c81766a5d9d15c11330d
Hannah Sättele <hannah.saettele@hs-furtwangen.de>
2021-05-13 22:30:23 +0200

    a8 und linter

---
+++ Aufgaben/Aufgabe8/script8.js
@@ -0,0 +1,5 @@
+var tmp = 1;
+if (tmp != 1) {
+    console.log('Die Bedingung ist wahr');
+}
+//# sourceMappingURL=script8.js.map
---
+++ Aufgaben/Aufgabe8/script8.ts
@@ -0,0 +1,5 @@
+var tmp: number = 1;
+
+if (tmp !=1) {
+    console.log('Die Bedingung ist wahr');
+}
